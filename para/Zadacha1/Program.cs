090d9ca3566e2ffebb6f3a18b1db5979ced956b1
Ivan <i.shumnovvvvv@gmail.com>
2023-01-27 15:25:23 +0300

    Lesson2

---
+++ para/Zadacha1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12-> 2
+85 -> 8
+
+1 - Ввыводим случайное число из отрезка 10 - 99.
+2 - показываем наибольшую цифру.*/
+
+int GetRandomNumber(int leftBound, int rightBound)
+{
+    int result = 0;
+    Random rnd = new Random();
+    result = rnd.Next(leftBound, rightBound + 1);
+    return result;
+}
+
+int GetMaxDigitOfNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secontDigit = number % 10;
+    if(firstDigit > secontDigit)
+        return firstDigit;
+    else
+        return secontDigit;
+}
+int number = GetRandomNumber(10, 99);
+int MaxDigit = GetMaxDigitOfNumber(number);
+Console.WriteLine($"Максимальная цифра числа {number} = {MaxDigit}");
---
+++ para/Zadacha2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+
+1.Создаем рандомное трехзначное число
+2.Удаляем вторую цифру этого числа*/
+
+int GetRandomValue()
+{
+    return new Random().Next(100, 999);
+}
+
+int DeliteTwoNumber(int Number)
+{
+    return (Number / 100) * 10 + (Number % 10);
+}
+
+int Number = GetRandomValue();
+int result = DeliteTwoNumber(Number);
+
+Console.WriteLine($"Загаданное число {Number}, стало {result}");
+
